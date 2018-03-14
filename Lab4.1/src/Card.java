d7e7b94714490bea7944573e18b697a2c56d63a5
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-03-14 09:20:51 -0400

    dfhed

--- Lab4.1/src/Card.java
+++ Lab4.1/src/Card.java
@@ -1,5 +1,44 @@
 
 public class Card 
 {
+	//fields
+	private String[] rank;
+	private String[] suit;
+	private int[] pointValue;
+	
+	//number 3
+	//check if this card equals another in value
+	public boolean equals(Card otherCard)
+	{
+		if(this.Card = otherCard)		//how do you call this card
+		{
+			return true;
+		}
+	}
+	
+	//getters and setters
+	public String[] getRank() {
+		return rank;
+	}
+	
+	public void setRank(String[] rank) {
+		this.rank = rank;
+	}
 
+	public String[] getSuit() {
+		return suit;
+	}
+	
+	public void setSuit(String[] suit) {
+		this.suit = suit;
+	}
+	
+	public int[] getPointValue() {
+		return pointValue;
+	}
+	
+	public void setPointValue(int[] pointValue) {
+		this.pointValue = pointValue;
+	}
+	
 }
--- Lab4.1/src/Deck.java
+++ Lab4.1/src/Deck.java
@@ -1,5 +1,14 @@
 
 public class Deck 
 {
-
+	public Deck(String[] rank, String[] suit, int[] value)
+	{
+		for (int i = 0; i < rank.length; i++) 
+		{
+			for (int j = 0; j < suit.length; j++)
+			{
+				unDealt.add(new Card(rank[i], suit[j], value[i]));
+			}
+		}
+	}
 }
