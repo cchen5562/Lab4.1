2b6a01f04af9a671576ac25a2740601d8cfe8e6c
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-03-15 09:16:23 -0400

    dfhbd

--- Lab4.1/src/Card.java
+++ Lab4.1/src/Card.java
@@ -2,42 +2,62 @@
 public class Card 
 {
 	//fields
-	private String[] rank;
-	private String[] suit;
-	private int[] pointValue;
+	private String rank;
+	private String suit;
+	private int pointValue;
+	
+	Card(String rank, String suit, int pointValue) 
+	{
+		this.rank = rank;
+		this.suit = suit;
+		this.pointValue = pointValue;
+	}
 	
 	//number 3
 	//check if this card equals another in value
 	public boolean equals(Card otherCard)
 	{
-		if(this.Card = otherCard)		//how do you call this card
+		if(otherCard.getPointValue() == this.pointValue)		
 		{
 			return true;
-		}
+		} 
+		return false;
+	}
+	
+	//return an appropriate string
+	public String toString() 
+	{
+		return rank + ", " + suit + ", " + pointValue;
 	}
 	
 	//getters and setters
-	public String[] getRank() {
+	public String getRank() 
+	{
 		return rank;
 	}
 	
-	public void setRank(String[] rank) {
+	public void setRank(String rank) 
+	{
 		this.rank = rank;
 	}
 
-	public String[] getSuit() {
+	public String getSuit() 
+	{
 		return suit;
 	}
 	
-	public void setSuit(String[] suit) {
+	public void setSuit(String suit) 
+	{
 		this.suit = suit;
 	}
 	
-	public int[] getPointValue() {
+	public int getPointValue() 
+	{
 		return pointValue;
 	}
 	
-	public void setPointValue(int[] pointValue) {
+	public void setPointValue(int pointValue) 
+	{
 		this.pointValue = pointValue;
 	}
 	
--- Lab4.1/src/Deck.java
+++ Lab4.1/src/Deck.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 
 public class Deck 
 {
+	//field
+	ArrayList<Card> unDealt;
+	ArrayList<Card> Dealt;
+
+	//the constructor
 	public Deck(String[] rank, String[] suit, int[] value)
 	{
 		for (int i = 0; i < rank.length; i++) 
@@ -11,4 +17,39 @@ public class Deck
 			}
 		}
 	}
+	
+	//return true when size of deck is 0
+	public boolean isEmpty()
+	{
+		if((unDealt.size() == 0) && (Dealt.size() == 0))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	//returns the number of cards left to be dealt
+	public int size()
+	{
+		return unDealt.size();
+	}
+	
+	//removes a card from the deck and returns it
+	//null if deck is empty
+	//dealt card should be added to dealt list and returned
+	public Card deal(Card x)
+	{
+		if(isEmpty() == true)
+		{
+			return null;
+		}
+		else 
+		{
+			unDealt.remove(x); 
+			return x;
+			Dealt.add(x);		//whyyy
+			return x;
+		}
+	}
+	
 }
